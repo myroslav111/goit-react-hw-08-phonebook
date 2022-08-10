80b0b4e8d97bfb8fe17a339ebe73d66c9bf46cb7
myroslav <myrkozar@gmail.com>
2022-08-10 18:39:37 +0300

    add hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/goit-react-hw-07-phonebook/">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <BrowserRouter basename="/goit-react-hw-07-phonebook/">
+//     <App />
+//   </BrowserRouter>
+// );
